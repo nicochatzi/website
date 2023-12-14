95e8d1848029211d0e9dd120a8fb69f6caae9e22
Nico Chatzi <nico.chatzigianis@focusrite.com>
2023-12-14 14:18:17 +0000

    add gtag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,21 +2,25 @@ import "@/styles/globals.css";
 import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
 import { Header } from "@/components/Header";
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider
-      defaultTheme="system"
-      attribute="class"
-    >
-      <div className="flex flex-col max-w-3xl mx-auto min-h-full px-4">
-        <Header />
-        <main id="main">
-          <Component {...pageProps} />
-        </main>
-        <footer className="py-16" />
-      </div>
-    </ThemeProvider >
+    <>
+      <GoogleAnalytics trackPageViews={{ ignoreHashChange: true }} />
+      <ThemeProvider
+        defaultTheme="system"
+        attribute="class"
+      >
+        <div className="flex flex-col max-w-3xl mx-auto min-h-full px-4">
+          <Header />
+          <main id="main">
+            <Component {...pageProps} />
+          </main>
+          <footer className="py-16" />
+        </div>
+      </ThemeProvider >
+    </>
   );
 }
 
