93aa51dd5b49a75e184bb00e610c814394f7e3d3
Nico Chatzi <nico.chatzigianis@focusrite.com>
2023-12-29 08:50:52 +0100

    update dependencies

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,17 +1,7 @@
 import type { NextPage } from "next";
 import { Page } from "@/components/Page";
 import { Prose } from "@/components/Prose";
-import { GitHub, Linkedin } from "react-feather";
-import siteConfig from "@/data/siteConfig";
-import { cx } from "@/lib/utils";
 import Socials from "@/components/Socials";
-
-const iconProps = { className: "w-4 h-4" };
-
-const SOCIAL_ICONS: { [key: string]: React.ReactNode } = {
-  github: <GitHub {...iconProps} />,
-  linkedin: <Linkedin {...iconProps} />,
-};
 
 const About: NextPage = () => {
   return (
